b91016dbac5ef7fbb1f955ad53d3e4801273c5ad
Rezaul Karim <shuvothesuperman@gmail.com>
2023-03-04 11:48:02 +0600

    update config app file

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('Mediusware Question', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'env' => env('APP_ENV', 'production'),
+    'env' => env('base64:wVxTIDVOOrRMtmbzL2fUUQLqtEGiKv/BzRY5gD6jLpk=', 'production'),
 
     /*
     |--------------------------------------------------------------------------
